d66552f518c183d99b7f2bed3fce3b7f0ffb4d82
Aaron Siddhartha Mondal <aaron@eomii.org>
2022-04-08 21:51:29 +0200

    😳 Rename site to docs

--- build_website.sh
+++ build_website.sh
@@ -1,18 +1,13 @@
 #/bin/sh
 
-# Build docs_source.
-# bazel build //ll:docs
-# chmod 644 bazel-bin/ll/*.md
-# cp bazel-bin/ll/*.md docs_source
-
 # Rebuild the Sphinx documentation.
-rm -rd site
-sphinx-build -b html src site
+rm -rd docs
+sphinx-build -b html src docs
 
 # Rerun the pre-commit hooks so that we do not need to stage everything twice.
-git add site
+git add docs
 pre-commit run --all-files
-git add site
+git add docs
 
 # Technically unnecessary, but we want to show users whether all tests pass.
 echo ""
